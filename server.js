a8ccb33b944c2d2af215a95d4fe1919c83004e39
ahmad1091 <ahmadtayeb91@gmail.com>
2022-08-21 16:17:20 +0300

    new changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
 const HOST = '0.0.0.0';
 
 // App
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT);
 console.log(`Running on http://${HOST}:${PORT}`);
